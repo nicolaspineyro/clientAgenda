bea6074b502375cb14ec4f76b6758077298f8e14
Nicolas Piñeyro <nicolaspineyro@users.noreply.github.com>
2021-01-04 14:16:23 -0300

    'ok'

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <Text>Hola Mundo!</Text>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
