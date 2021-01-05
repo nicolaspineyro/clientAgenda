ade863d61a1eb75a81ee3e5215d05a8d2d15127d
Nicolas Piñeyro <nicolaspineyro@users.noreply.github.com>
2021-01-04 22:49:47 -0300

    Getting more done

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 import Home from './views/Home';
 import NewClient from './views/NewClient';
 import ClientDetails from './views/ClientDetails';
 
+import HeaderBar from './components/ui/HeaderBar';
+
+
 const Stack = createStackNavigator();
+const theme = {
+  ...DefaultTheme
+}
+
+console.log(theme);
 
 const App = () => {
   return (
@@ -23,10 +27,24 @@ const App = () => {
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName='Home'
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: theme.colors.primary
+            },
+            headerTintColor: theme.colors.surface
+          }}
         >
           <Stack.Screen
             name='Home'
             component={Home}
+            options={({ navigation, route }) => ({
+              headerTitleAlign: 'center',
+              headerLeft: (props) => <HeaderBar
+                navigation={navigation}
+                route={route}
+              />
+            })}
+
           />
           <Stack.Screen
             name='New Client'
---
+++ components/ui/HeaderBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'react-native-paper';
+
+const HeaderBar = ({ navigation, routes }) => {
+    return (
+        <Button style={{backgroundColor:'#fff'}} onPress={() => navigation.navigate('New Client')}>
+            New Client
+        </Button>
+    )
+}
+
+export default HeaderBar;
